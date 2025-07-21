c65874f77877af0259ece96e929d694c5b8858df
Muhammad Anas <129672108+MuhammadAnasAI@users.noreply.github.com>
2025-07-21 14:05:56 +0500

    Static Web App

--- main.py
+++ main.py
@@ -18,3 +18,23 @@ async def root():
 @app.get("/survival_rate")
 async def get_survival_rate():
     return survival_rate.to_dict(orient="records")
+#Create a rotue for survival rate plot:
+@app.get("/survival_rate_plot", response_class=HTMLResponse)
+async def get_survival_rate_plot():
+    #fig, ax = plt.subplots()
+    sns.barplot(x="class", y="survived", hue="sex",  data=df)
+    #labels of plot:
+    plt.title('Survival Rate by Class')
+    plt.xlabel('Class')
+    plt.ylabel('Survival Rate')
+    #Save the plot to a bytes buffer:
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    plt.close()
+    buf.seek(0)
+    # Encode the plot as a base64-encoded string:
+    image_str = base64.b64encode(buf.getbuffer()).decode("utf-8")
+    #Generate the HTML content:
+    html_content = f'<img src="data:image/png;base64, {image_str}" alt="Survived Image">'
+    return HTMLResponse(content=html_content)
+
