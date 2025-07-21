cc1141dfcf67e8d244ada90204d3276102c37cdf
Muhammad Anas <129672108+MuhammadAnasAI@users.noreply.github.com>
2025-07-21 14:01:25 +0500

    Fast Web App

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
+import json
+import os
+from typing import Literal, Optional
+from uuid import uuid4
 from fastapi import FastAPI, HTTPException
-import seaborn as sns
-import pandas as pd
-from fastapi.responses import HTMLResponse
-import matplotlib.pyplot as plt
-import plotly.express as px
-from plotly.io import to_html
+import random
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from mangum import Mangum 
 
+class Book(BaseModel):
+    name:str
+    genre:Literal["fiction", "non fiction"]
+    price:float
+    book_id:Optional[str] = uuid4().hex
+BOOKS_FILE = "books.json"
+BOOKS = []
 
+if os.path.exists(BOOKS_FILE):
+    with open(BOOKS_FILE, "r") as f:
+        BOOKS = json.load(f)
 
 app = FastAPI()
-df = sns.load_dataset("titanic")
-#Perform a simple data transformation:
-survival_rate = df.groupby("class")['survived'].mean().reset_index()
-#Define the route:
+handler = Mangum(app)
+
 @app.get("/")
 async def root():
-    return {"message": "This is a titanic web app route!"}
-@app.get("/survival_rate")
-async def get_survival_rate():
-    return survival_rate.to_dict(orient="records")
-#Create a rotue for survival rate plot:
-@app.get("/survival_rate_plotly", response_class=HTMLResponse)
-async def get_survival_rate_plotly():
-    #plot the dataset:
-    fig = px.bar(df, x="class", y="survived", color="sex")
-    #Convert the plot into the html:
-    html = to_html(fig, full_html=False)
-    #Create the full html document:
-    full_html = f"""
-    <html>
-    <head>
-    <title>Survival Rate Plot</title>
-    </head>
-    <body>
-    <h1>Survival Rate Plot</h1>
-    {html}
-    </body>
-    </html>
-    """
-    return HTMLResponse(content=full_html)
-#Specify the ports to run the app at 5000:
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
-                
+    return{"message": "welcom to my book store app!"}
+@app.get("/random-books")
+async def random_book():
+    return random.choice(BOOKS)
+@app.get("/list-books")
+async def list_books():
+    return {"books":BOOKS}
+@app.get('/book_by_index/{index}')
+async def book_by_index(index: int):
+    if index < len(BOOKS):
+        return BOOKS[index]
+    else:
+        raise HTTPException(404, f"BOOK ID {index} not found into database!({len(BOOKS)})")
+@app.post("/add-book")
+async def add_book(book: Book):
+    book.book_id = uuid4().hex
+    json_book = jsonable_encoder(book)
+    BOOKS.append(json_book)
+    with open(BOOKS_FILE, "w") as f:
+        json.dump(BOOKS, f)
+    return {"book_id":book.book_id}
+@app.get("/get-book")
+async def get_book(book_id: str):
+    for book in BOOKS:
+        if book["book_id"] == book_id:
+            return book
+        raise HTTPException(404, f"BOOK ID {book_id} not found into database!")
