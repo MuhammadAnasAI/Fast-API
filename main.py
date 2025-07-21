e7cd4212ced53920b5a8e76a8530a7c15d075223
Muhammad Anas <129672108+MuhammadAnasAI@users.noreply.github.com>
2025-07-21 14:08:00 +0500

    parameter app

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
 from fastapi import FastAPI
-import seaborn as sns
-import pandas as pd
-from fastapi.responses import HTMLResponse
-import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
-
-
 app = FastAPI()
-df = sns.load_dataset("titanic")
-#Perform a simple data transformation:
-survival_rate = df.groupby("class")['survived'].mean().reset_index()
-#Define the route:
 @app.get("/")
-async def root():
-    return {"message": "This is a titanic web app route!"}
-@app.get("/survival_rate")
-async def get_survival_rate():
-    return survival_rate.to_dict(orient="records")
-#Create a rotue for survival rate plot:
-@app.get("/survival_rate_plot", response_class=HTMLResponse)
-async def get_survival_rate_plot():
-    #fig, ax = plt.subplots()
-    sns.barplot(x="class", y="survived", hue="sex",  data=df)
-    #labels of plot:
-    plt.title('Survival Rate by Class')
-    plt.xlabel('Class')
-    plt.ylabel('Survival Rate')
-    #Save the plot to a bytes buffer:
-    buf = BytesIO()
-    plt.savefig(buf, format="png")
-    plt.close()
-    buf.seek(0)
-    # Encode the plot as a base64-encoded string:
-    image_str = base64.b64encode(buf.getbuffer()).decode("utf-8")
-    #Generate the HTML content:
-    html_content = f'<img src="data:image/png;base64, {image_str}" alt="Survived Image">'
-    return HTMLResponse(content=html_content)
-
+async def read_root():
+    return {"message": "Hello, I am a Data Scientist working on the Artificial Intelligence."}
+@app.post("/")
+async def post_root():
+    return {"message": "Hello, I am an expert in Artficial Intelligence."}
+#@app.put("/{item_id}")
+#async def put_item(item_id: int):
+ #   return{"message": f"item ID is {item_id}"}
+#Add a path parameter:
+@app.get("/items")
+async def get_items():
+    return {"message": "This is an item_id"}
+#Check the path Parameters for the Items:
+@app.get("/items/{item_id}")
+async def get_item(item_id):
+    return {"item_id": item_id}
+#Check tht path parameters for the Users:
+@app.get("/users/{user_id}")
+async def get_item(user_id):
+    return {"item_id": user_id}
